d7abe7e06cc85d2b6fe2f762cf991004d3a9d40f
Impre-visible <impr.visible@gmail.com>
2024-04-04 19:39:09 +0200

    Feat: On scroll, update the headbar to add the class active to the good <a>

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -16,6 +16,40 @@
     headerLinks.forEach((link) => link.classList.remove("active"));
     link.classList.add("active");
   }
+
+  const ids = ["home", "github", "showcase", "skills", "socials"];
+
+  onscroll = () => {
+    const scroll = window.scrollY;
+    const sections = [];
+    for (let i = 0; i < ids.length; i++) {
+      if (!document.getElementById(ids[i])) {
+        continue;
+      }
+      sections.push(document.getElementById(ids[i]));
+    }
+    if (!sections) {
+      return;
+    }
+    const offsets = sections.map((section) => section.offsetTop - 100);
+
+    for (let i = 0; i < sections.length; i++) {
+      if (!sections[i]) {
+        continue;
+      }
+      if (scroll >= offsets[i] && scroll < offsets[i + 1]) {
+        const headerLinks = document.querySelectorAll(".header a");
+        headerLinks.forEach((link) => link.classList.remove("active"));
+        headerLinks[i].classList.add("active");
+      }
+
+      if (scroll >= offsets[sections.length - 1]) {
+        const headerLinks = document.querySelectorAll(".header a");
+        headerLinks.forEach((link) => link.classList.remove("active"));
+        headerLinks[sections.length - 1].classList.add("active");
+      }
+    }
+  };
 </script>
 
 <div class="headerContainer">
